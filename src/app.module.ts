57b6b110c0162202b79b54cf1841e372e7ca53bb
NascimentoTalesDev <111396464+NascimentoTalesDev@users.noreply.github.com>
2024-10-30 15:02:15 -0300

    feat: init

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, UsersModule],
   controllers: [AppController],
   providers: [AppService]
 })
